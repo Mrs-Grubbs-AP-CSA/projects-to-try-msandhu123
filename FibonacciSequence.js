bfe45169c477349693fce69209bffe402a66aa84
msandhu123 <msandhu1@apps.district833.org>
2024-06-01 13:29:55 -0500

    Update FibonacciSequence.js

--- FibonacciSequence.js
+++ FibonacciSequence.js
@@ -4,25 +4,26 @@ public class FibonacciSequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number of terms for fibonacci sequence: ");
-        int n = console.nextInt();
-        System.out.println("Fibonnaci Sequence up to the " + n + "th term: " + sequence(n));
+        int n = scanner.nextInt();
+        String numbers = sequence(n);
+        System.out.println("Fibonnaci Sequence up to the " + n + "th term: " + numbers);
     }
 
-    public String sequence(int num){
+    public static String sequence(int num){
         if (num == 1){
             return "1";
         } else if (num == 2){
             return "1, 1";
         }
-        String output = "1, 1"
+        String output = "1, 1";
         int num1 = 1;
-        int num2 = 2;
+        int num2 = 1;
         for (int i = 0; i < num - 2; i++){
             int num3 = num1 + num2;
             num1 = num2;
             num2 = num3;
-            output = output + " " + num2 + ",";
+            output = output + ", " + num2;
         }
         return output;
     }
-
+}
