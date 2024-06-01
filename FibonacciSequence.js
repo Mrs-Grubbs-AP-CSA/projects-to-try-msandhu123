05e919f9ba7cc99fb324d1311cc6d0f880e698a9
msandhu123 <msandhu1@apps.district833.org>
2024-06-01 12:49:07 -0500

    Update and rename FibonacciSequence to FibonacciSequence.js

---
+++ FibonacciSequence.js
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class FibonacciSequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of terms for fibonacci sequence: ");
+        int n = console.nextInt();
+        System.out.println("Fibonnaci Sequence up to the " + n + "th term: " + sequence(n));
+    }
+
+    public String sequence(int num){
+        if (num == 1){
+            return "1";
+        } else if (num == 2){
+            return "1, 1";
+        }
+        String output = "1, 1"
+        int num1 = 1;
+        int num2 = 2;
+        for (int i = 0; i < num - 2; i++){
+            int num3 = num1 + num2;
+            num1 = num2;
+            num2 = num3;
+            output = output + " " + num2 + ",";
+        }
+        return output;
+    }
+
